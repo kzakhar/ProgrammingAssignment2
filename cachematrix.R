02bc35c41324b4de3053ce8fb509ffb9bba6fadb
Konstantin <master.zakharov@gmail.com>
2024-07-08 22:57:48 +0200

    feat(scoping): caching the inverse of a matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+## creates a custom matrix-"object" with the functions to cache an inverted matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+  # set the initial value for an "inverted" matrix to NULL
+  inv <- NULL
+  # function to set the matrix
+  set <- function(y) {
+    x <<- y
+    # reset the "inverted" matrix
+    inv <<- NULL
+  }
+  # function to return the "original" matrix
+  get <- function() x
+  # function to set the the cached "inverted" matrix
+  setInverted <- function(inverted) inv <<- inverted
+  # function to get the the cached "inverted" matrix
+  getInverted <- function() inv
+  # return our functions in the list
+  list(set = set, get = get,
+       setInverted = setInverted,
+       getInverted = getInverted)
 }
 
 
-## Write a short comment describing this function
-
+## calculates the inverted matrix of the custom matrix-"object"
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverted()
+  # check if a cached value is available
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  # ...it's not
+  mat <- x$get()
+  # calculate the inverted matrix, propagating the input params (...) 
+  inv <- solve(mat, ...)
+  # update the cache
+  x$setInverted(inv)
+  # and return the result
+  inv
 }
